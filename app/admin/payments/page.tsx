1dafaf023e41d9d5979b4ba2b9e3238894aab976
tymyevhenmpw <139116341+tymyevhenmpw@users.noreply.github.com>
2024-09-20 11:43:40 +0300

    Adapted payments at admin panel

--- app/admin/payments/page.tsx
+++ app/admin/payments/page.tsx
@@ -7,7 +7,7 @@ const Page = async () => {
     const payments = await fetchOrdersPayments();
 
     return (
-        <section className="px-10 py-20 w-full">
+        <section className="px-10 py-20 w-full max-[360px]:px-4">
             <h1 className="text-heading1-bold drop-shadow-text-blue">Оплати</h1>
             <DataTable columns={columns} data={payments}/>
         </section>
